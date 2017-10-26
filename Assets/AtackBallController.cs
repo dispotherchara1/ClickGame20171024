6371063fe0a9d5dec53bbc53dc09a6d2c5db16c3
Rikiya Yamamoto <nkc.yamamto.rikiya@gmail.com>
2017-10-26 10:12:14 +0900

    AtackBallCon

--- Assets/AtackBallController.cs
+++ Assets/AtackBallController.cs
@@ -2,12 +2,19 @@
 using System.Collections;
 
 public class AtackBallController : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
+	public void Shoot(Vector3 dir)
+    {
+        GetComponent<Rigidbody>().AddForce(dir);
+    }
+    void OnCollisionEnter(Collision other)
+    {
+        GetComponent<Rigidbody>().isKinematic = true;
+    }
+    // Use this for initialization
+    void Start () {
+        Shoot(new Vector3(0, 200, 2000));
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 	
