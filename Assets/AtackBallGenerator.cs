96a8e779301ddfe6c483bc21c498517b5fd93c5c
Rikiya Yamamoto <nkc.yamamto.rikiya@gmail.com>
2017-10-26 10:22:18 +0900

    Reload Script

--- Assets/AtackBallGenerator.cs
+++ Assets/AtackBallGenerator.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class AtackBallGenerator : MonoBehaviour {
 
+    public GameObject AtackBallPrefab;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +12,12 @@ public class AtackBallGenerator : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if (Input.GetMouseButton(0))
+        {
+            GameObject AtackBall =
+                Instantiate(AtackBallPrefab) as GameObject;
+            AtackBall.GetComponent<AtackBallController>().Shoot(
+                new Vector3(0, 200, 2000));
+        }
 	}
 }
