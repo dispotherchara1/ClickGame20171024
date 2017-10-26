91f4e33623c2d07d137df2ea59e5e6d5878050df
Rikiya Yamamoto <nkc.yamamto.rikiya@gmail.com>
2017-10-26 10:24:49 +0900

    Create DeleteMan Script

---
+++ Assets/DeleteMan.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DeleteMan : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
